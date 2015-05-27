fixes bug in the computation of the hessian

RaschModel.py
@@ -121,7 +121,7 @@ def _rasch_alternating(Y, b, k, gamma, max_iter, tol, solver):
     def gradient(a): 
         return (mu - np.sum(1 / (1+np.exp(-(a + b)))))
     def hessian(a):
-        tmp = np.sum(1 / ((1 + np.exp(-(a + b))) * ((1 + np.exp(a + b)))))
+        tmp = -np.sum(1 / ((1 + np.exp(-(a + b))) * ((1 + np.exp(a + b)))))
         return(tmp)
     
     tolerance = tol + 1

tests/test_rasch_model.py
@@ -32,12 +32,14 @@ def test_learn_rasch_model():
     rm = RaschModel(a, b)
     Y = RaschModel(a, b).sample()
 
-    for solver in ['newton']:
+    for solver in ['gradient', 'newton']:
         a_est, b_est, num_iter = LearnRaschModel(solver=solver,
                                                  verbose=False,
-                                                 max_iter_inner=1000,
+                                                 max_iter_inner=100,
                                                  max_iter_outer=10) \
                                 .fit(Y)
         print(a_est)
+        print(b_est)
+        print(b)
         assert_true((a_est[0] < 0) & (a_est[1] > 0))
         assert_true(np.sum(a_est) == 0)