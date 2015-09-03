248871f9ef7339a77ec4b2c30beada0e97dec320
Divyanshu Vats <vats.div@gmail.com>
2015-09-03 07:41:53 -0400

    adds setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RaschModel',
+      version='0.1',
+      description='Rasch Model Learning',
+      author='Divyanshu Vats',
+      author_email='vats.div@gmail.com',
+      url='https://github.com/vats-div/rasch_model',
+      packages=['rasch_model']
+     )
